78abb8cffa140d3893a00c44c068a59f5bbd5dfe
Mhm0ud <m.h.m.almasri@hotmail.com>
2021-04-07 19:13:22 +0300

    typo

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -2,6 +2,6 @@
 
 # Decrypt the file
 mkdir $HOME/secrets
-# --batch to prevent interactive command --yes to assume "yes" for question
+# --batch to prevent interactive command --yes to assume "yes" for question 
 gpg --quiet --batch --yes --decrypt --passphrase="$PASS" \
 --output $HOME/secrets/token.json token.json.gpg
