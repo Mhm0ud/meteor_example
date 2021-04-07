1bd60ccf652a8509d660e8723f7801b875447ce7
Mhm0ud <m.h.m.almasri@hotmail.com>
2021-04-07 19:12:06 +0300

    encrypt json token

---
+++ .github/scripts/decrypt.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command --yes to assume "yes" for question
+gpg --quiet --batch --yes --decrypt --passphrase="$PASS" \
+--output $HOME/secrets/token.json token.json.gpg
